4a93c65ee37d0e13760c7a5965235ffec0097e4f
Bisma Muqeed <bismamuqeed@gmail.com>
2024-03-31 23:26:35 -0700

    created CLI-Number Guessing Game

---
+++ demo2.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let randomNumber = Math.floor(Math.random() * 6 + 1);
+
+const answer = await inquirer.prompt([{
+    name: "userGuessedNumber",
+    type: "number",
+    message: "Please Guess a Number Between 1 to 6 : ",
+},
+])
+
+
+if(answer.userGuessedNumber === randomNumber){
+    console.log("Congtratulations! You Guessed The Correct Number" );
+}
+else{
+    console.log("You Guessed The Wrong Number");
+    console.log(`Correct Guessed Number Is ${randomNumber}`);
+}
